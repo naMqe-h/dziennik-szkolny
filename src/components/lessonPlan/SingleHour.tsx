45438c5ac62da90adb78dae9b068018e64d5e1a5
naMqe <naMqe07@gmail.com>
2022-02-14 14:59:48 +0100

    changed emails to surnames, added spaces to subject names

--- src/components/lessonPlan/SingleHour.tsx
+++ src/components/lessonPlan/SingleHour.tsx
@@ -1,4 +1,7 @@
-import { singleHoursFromLessonPlan } from '../../utils/interfaces'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../redux/store'
+import { singleHoursFromLessonPlan, SingleTeacherData, SubjectData } from '../../utils/interfaces'
 
 interface SingleHourProps {
     isEmpty: boolean
@@ -6,14 +9,36 @@ interface SingleHourProps {
 }
 
 export const SingleHour: React.FC<SingleHourProps> = ({ lesson, isEmpty }) => {
+    const teachers = useSelector((state: RootState) => state.schoolData.schoolData?.teachers)
+    const subjects = useSelector((state: RootState) => state.schoolData.schoolData?.subjects)
+    const [teacher, setTeacher] = useState<SingleTeacherData>()
+    const [subject, setSubject] = useState<SubjectData>()
+
+    useEffect(() => {
+        if(teachers) {
+            for(const [key, value] of Object.entries(teachers)) {
+                if(key === lesson?.teacher) setTeacher(value)
+            }
+        }
+    }, [teachers, isEmpty, lesson?.teacher])
+
+    useEffect(() => {
+        if(subjects) {
+            for(const [key, value] of Object.entries(subjects)) {
+                if(key === lesson?.subject) setSubject(value)
+            }
+        }
+    }, [subjects, isEmpty, lesson?.subject])
+
     return (
         <td className="border-2 border-base-200">
             <div>
                 <h1 className="text-lg font-bold">
-                    {isEmpty ? '' : lesson?.subject}
+                    {isEmpty ? '' : subject?.name}
+                    {lesson?.subject === 'GodzinaWychowawcza' && 'Godzina wychowawcza'}
                 </h1>
                 <span className="text-sm text-gray-500">
-                    {isEmpty ? '' : lesson?.teacher}
+                    {isEmpty ? '' : `${teacher?.lastName || ''} ${teacher?.firstName || ''}`}
                 </span>
             </div>
         </td>
